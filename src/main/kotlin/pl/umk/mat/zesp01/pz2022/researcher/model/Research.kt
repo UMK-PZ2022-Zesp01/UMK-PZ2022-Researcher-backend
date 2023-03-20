de28c0973dbd4344e8864f3de8655d9582c98e4e
Dawid Odolczyk <dawid.odolczyk@gmail.com>
2023-03-20 20:27:13 +0100

    S4-T02: Early version of Research Requirements functionality

--- src/main/kotlin/pl/umk/mat/zesp01/pz2022/researcher/model/Research.kt
+++ src/main/kotlin/pl/umk/mat/zesp01/pz2022/researcher/model/Research.kt
@@ -1,75 +1,132 @@
 package pl.umk.mat.zesp01.pz2022.researcher.model
 
+import com.fasterxml.jackson.core.type.TypeReference
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 import org.springframework.data.mongodb.core.mapping.Field
 
 @Document("Researches")
 class Research {
-    @Id var id: String = ""
-//    @Field var creatorId: String = ""
-    @Field var creatorLogin: String = ""
-    @Field var title: String = ""
-    @Field var description: String = ""
-    @Field var posterId: String = ""
-    @Field var participantLimit: Int = 0
-    @Field var participants = listOf<User>()
-    @Field var begDate: String = ""
-    @Field var endDate: String = ""
-    @Field var isActive: Boolean = false
-    @Field var location: ResearchLocation? = null
-    @Field var rewards = listOf<ResearchReward>()
-//    @Field var requirement = listOf<ResearchRequirement>()
+	@Id var id: String = ""
+	//    @Field var creatorId: String = ""
+	@Field var creatorLogin: String = ""
+	@Field var title: String = ""
+	@Field var description: String = ""
+	@Field var posterId: String = ""
+	@Field var participantLimit: Int = 0
+	@Field var participants = listOf<User>()
+	@Field var begDate: String = ""
+	@Field var endDate: String = ""
+	@Field var isActive: Boolean = false
+	@Field var location: ResearchLocation? = null
+	@Field var rewards = listOf<ResearchReward>()
+	@Field var requirements = listOf<ResearchRequirement>()
 }
 
 class ResearchResponse(
-    var id: String,
-    var researchCreatorId: String
-    // TODO
+	var id: String,
+	var researchCreatorId: String
+	// TODO
 )
 
 class ResearchRequest(
-    val title: String,
-    val description: String,
-    val creatorLogin: String,
-    val posterId: String,
-    val begDate: String,
-    val endDate: String,
-    val participantLimit: Int,
-    val location: ResearchLocation,
-    val rewards: List<ResearchReward>
+	private val title: String,
+	private val description: String,
+	private val creatorLogin: String,
+	private val posterId: String,
+	private val begDate: String,
+	private val endDate: String,
+	private val participantLimit: Int,
+	private val location: ResearchLocation,
+	private val rewards: List<ResearchReward>,
+	private val requirements: List<ResearchRequirement>
 ) {
-    fun toResearch(): Research {
-        val research = Research()
+	fun toResearch(): Research {
+		val research = Research()
 
-        research.title = this.title
-        research.description = this.description
-        research.creatorLogin = this.creatorLogin
-        research.posterId = this.posterId
-        research.begDate = this.begDate
-        research.endDate = this.endDate
-        research.participantLimit = this.participantLimit
-        research.location = this.location
-        research.rewards = this.rewards
+		research.title = this.title
+		research.description = this.description
+		research.creatorLogin = this.creatorLogin
+		research.posterId = this.posterId
+		research.begDate = this.begDate
+		research.endDate = this.endDate
+		research.participantLimit = this.participantLimit
+		research.location = this.location
 
-        return research
-    }
+		research.rewards = this.rewards.map { reward ->
+			when (reward.type) {
+				"cash" -> ResearchReward(reward.type, reward.value.toString().toFloat())
+				"item" -> ResearchReward(reward.type, reward.value as String)
+				else -> ResearchReward("", "")
+			}
+
+		}
+
+		research.requirements = this.requirements.map { req ->
+			when (req.type) {
+				"gender" -> ResearchRequirement(
+					req.type,
+					ObjectMapper().convertValue(
+						req.criteria,
+						object : TypeReference<List<String>>() {}
+					)
+				)
+
+//                "age" -> ResearchRequirement(req.type, req.criteria as ResearchRequirementAge)
+
+				"place" -> ResearchRequirement(
+					req.type,
+					ObjectMapper().convertValue(
+						req.criteria,
+						object : TypeReference<List<String>>() {}
+					)
+				)
+
+				"education" -> ResearchRequirement(
+					req.type,
+					ObjectMapper().convertValue(
+						req.criteria,
+						object : TypeReference<List<String>>() {}
+					)
+				)
+
+				"marital" -> ResearchRequirement(
+					req.type,
+					ObjectMapper().convertValue(
+						req.criteria,
+						object : TypeReference<List<String>>() {}
+					)
+				)
+
+				else -> ResearchRequirement("", "")
+			}
+		}
+
+		return research
+	}
 }
 
 class ResearchLocation(
-    var form: String = "",
-    var place: String = ""
+	var form: String = "",
+	var place: String = ""
 )
 
 class ResearchReward(
-    var type: String = "",
-    var value: String = ""
+	var type: String = "",
+	var value: Any
 )
 
 class ResearchRequirement(
-    var type: String = "",
-    var ageMin: Int = -1,
-    var ageMax: Int = -1,
-    var genderNames: String = "",
-    var custom: String = ""
+	var type: String = "",
+	var criteria: Any
+)
+
+class ResearchRequirementAgeInterval(
+	var ageMin: Int? = null,
+	var ageMax: Int? = null
+)
+
+class ResearchRequirementOther(
+	var description: String
 )
